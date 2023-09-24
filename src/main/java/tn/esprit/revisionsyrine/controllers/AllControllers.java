dd9a219726925b23294d0f94c9a08c088385bb3d
jihedmakthri <92696670+jihedmakthri@users.noreply.github.com>
2023-09-25 00:24:22 +0100

    test automatique run build jenkins 7

--- src/main/java/tn/esprit/revisionsyrine/controllers/AllControllers.java
+++ src/main/java/tn/esprit/revisionsyrine/controllers/AllControllers.java
@@ -24,7 +24,7 @@ public class AllControllers {
 
     @PostMapping("/hello")
     public String helloTest(){return "hello world !!";}
-    //skjdfsqsdgsdfsdgfsh
+    //skjdfsqsdgsdfsdgsdfsdfsh
     @PostMapping("/event/addandaffect")
     public Evenement ajoutAffectEvenParticip (@RequestBody Evenement e){
         return allServices.ajoutAffectEvenParticip(e);
