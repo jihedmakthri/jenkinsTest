738b7a5fac1faa87fea15ebc55f8c61fc187853d
jihedmakthri <92696670+jihedmakthri@users.noreply.github.com>
2023-09-29 15:44:31 +0100

    mohssen8

--- src/main/java/tn/esprit/revisionsyrine/controllers/AllControllers.java
+++ src/main/java/tn/esprit/revisionsyrine/controllers/AllControllers.java
@@ -24,7 +24,7 @@ public class AllControllers {
 
     @PostMapping("/hello")
     public String helloTest(){return "hello world !!";}
-    //skjdfsqsdsrgerhsgsgsdgslriumkj54
+    //skjdfsqsdsrgerhsslriumkj54
     @PostMapping("/event/addandaffect")
     public Evenement ajoutAffectEvenParticip (@RequestBody Evenement e){
         return allServices.ajoutAffectEvenParticip(e);
